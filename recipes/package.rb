42871f4adeea7582e56286643dd441b274ae9a71
Franklin Hanson <fhanson@gannett.com>
2014-11-22 15:06:54 -0500

    Update install location for packages

--- recipes/package.rb
+++ recipes/package.rb
@@ -2,8 +2,13 @@
 # Cookbook Name:: jq
 # Recipe:: pacakge
 #
-
-remote_file "/usr/local/bin/jq" do
-  source node['jq']['package_url']
-  mode 0755
+if node["platform_family"] == "windows"
+  remote_file "#{node['jq']['install_dir']}\\jq.exe" do
+    source node['jq']['package_url']
+  end
+else
+  remote_file "#{node['jq']['install_dir']}\\jq" do
+  	source node['jq']['package_url']
+  	mode 0755
+  end
 end
